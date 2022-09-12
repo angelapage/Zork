22645b2a4d56578a6becad8ddf96b102ed5c4a31
angelapage <angelapage1617@gmail.com>
2022-09-12 14:26:52 -0400

    Zork 2.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -11,7 +11,7 @@ namespace Zork
             bool isRunning = true;
             while (isRunning)
             {
-                Console.Write($"{_rooms[_currentRoom]}\n >");
+                Console.Write($"{_rooms[_currentRow, _currentColumn]}\n >");
                 string inputString = System.Console.ReadLine();
                 Commands command = ToCommand(inputString.Trim());
 
@@ -27,9 +27,9 @@ namespace Zork
                         outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
 
-                    case Commands.NORTH:                        
-                    case Commands.SOUTH:                 
-                    case Commands.EAST:                       
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
                     case Commands.WEST:
                         if (Move(command))
                         {
@@ -40,22 +40,22 @@ namespace Zork
                         {
                             outputString = "The way is shut!";
                         }
-                        
-                       
+
+
                         break;
-                   
+
                     default:
                         outputString = "Unknown command.";
                         break;
                 }
 
                 Console.WriteLine(outputString);
-            }                     
+            }
         }
 
         private static Commands ToCommand(string commandString)
-        {                  
-            return Enum.TryParse(commandString,true,out Commands result) ? result : Commands.UNKNOWN;
+        {
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
         }
 
         private static bool Move(Commands command)
@@ -64,24 +64,40 @@ namespace Zork
 
             switch (command)
             {
-                case Commands.NORTH:
-                case Commands.SOUTH:                   
-                    break;
-                case Commands.EAST when _currentRoom < _rooms.Length - 1:
-                    _currentRoom++;
-                    didMove = true;                                      
+                case Commands.NORTH when _currentRow < _rooms.GetLength(0) - 1:
+                    _currentRow++;
+                    didMove = true;
                     break;
 
-                case Commands.WEST when _currentRoom > 0:                    
-                    _currentRoom--;
-                    didMove = true;                                   
-                    break;                    
+                case Commands.SOUTH when _currentRow > 0:
+                    _currentRow--;
+                    didMove = true;
+                    break;
+
+                case Commands.EAST when _currentColumn < _rooms.GetLength(1) - 1:
+                    _currentColumn++;
+                    didMove = true;
+                    break;
+
+                case Commands.WEST when _currentColumn > 0:
+                    _currentColumn--;
+                    didMove = true;
+                    break;
             }
 
             return didMove;
-        } 
+        }
 
-        private static readonly string[] _rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
-        private static int _currentRoom = 1;
+        private static readonly string[,] _rooms =
+            {
+
+                {"Rocky Trail", "South of House", "Canyon View" },
+                {"Forest", "West of House", "Behind House" },
+                {"Dense Woods", "North of House", "Clearing" }
+            
+            };
+
+        private static int _currentRow = 1;
+        private static int _currentColumn = 1;
     }   
 }
