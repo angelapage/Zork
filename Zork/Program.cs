812d16732f48e4ef05d06813a804d0e129b74597
angelapage <angelapage1617@gmail.com>
2022-09-14 13:35:41 -0400

    Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,7 +4,7 @@ namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -13,6 +13,8 @@ namespace Zork
         }
         static void Main(string[] args)
         {
+            InitalizeRomDescriptions();
+
             Console.WriteLine("Welcome to Zork!");
 
             bool isRunning = true;
@@ -31,7 +33,7 @@ namespace Zork
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        outputString = CurrentRoom.Description;
                         break;
 
                     case Commands.NORTH:
@@ -95,12 +97,27 @@ namespace Zork
             return didMove;
         }
 
-        private static readonly string[,] _rooms =
+        private static void InitalizeRomDescriptions()
+        {
+            _rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            _rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            _rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            _rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            _rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door";
+            _rooms[1, 2].Description = "You are behind a white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            _rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            _rooms[2, 1].Description = "You are facing the north side of a white house. There is no ddor here, and all the windows are barred.";
+            _rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";        
+        }
+
+        private static readonly Room[,] _rooms =
             {
 
-                {"Rocky Trail", "South of House", "Canyon View" },
-                {"Forest", "West of House", "Behind House" },
-                {"Dense Woods", "North of House", "Clearing" }
+                {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+                {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+                {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
             
             };
 
---
+++ Zork/Rooms.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork
+{
+    class Room
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Room(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
